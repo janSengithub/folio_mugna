1a99d281303623dbb4bccafb714abb72fcd136a8
janSengithub <jansen.solayao@bipsu.edu.ph>
2025-03-25 17:35:25 +0800

    Update project with new code

--- Documents/folio/src/app/components/Projects.js
+++ Documents/folio/src/app/components/Projects.js
@@ -1,25 +1,25 @@
 "use client";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const slides = [
   {
-    src: "/img/achievements/awardwinning.jpg",
+    src: "/img/proj/206-coverpage.jpg",
     title: "Short Competition",
     description:
       "My second time joining a short film competition and held as champion and bagged several minor awards.",
   },
   {
-    src: "/img/achievements/iset.jpg",
+    src: "/img/proj/ISITE.jpg",
     title: "ISITE",
     description: "Success is best when it is shared by the team.",
   },
   {
-    src: "/img/achievements/Researchlympics.jpg",
+    src: "/img/proj/researchlymp.jpg",
     title: "Researchlympics",
     description: "Awarded as 3rd in a University-wide competition.",
   },
   {
-    src: "/img/achievements/MEVAW.jpg",
+    src: "/img/proj/gadvocacy.jpg",
     title: "Advocacy",
     description: "Held as the champion.",
   },
@@ -36,14 +36,24 @@ export default function ImageSlider() {
     setCurrentIndex((prevIndex) => (prevIndex - 1 + slides.length) % slides.length);
   };
 
+    useEffect(() => {
+      const intervalId = setInterval(() => {
+        nextSlide();
+      }, 5000);
+
+      return () => clearInterval(intervalId);
+    }, []);
+
   return (
     <div className="relative flex flex-col md:flex-row justify-center items-center h-screen md:h-[600px] lg:h-[800px] overflow-visible rounded-lg mx-auto">
       <div className="relative max-w-lg md:max-w-md lg:max-w-lg text-gray-700">
-        <img
-          src={slides[currentIndex].src}
-          alt="Slider Image"
-          className="w-full h-full object-cover rounded-lg transition-opacity duration-300"
-        />
+        <div className="image-container w-full h-[500px] rounded-lg">
+          <img
+            src={slides[currentIndex].src}
+            alt="Slider Image"
+            className="w-full h-full object-cover rounded-lg transition-opacity duration-300"
+          />
+        </div>
       </div>
       <div className="relative max-w-lg md:max-w-md lg:max-w-lg text-gray-700 md:ml-16">
         <h1 className="w-[250px] text-start text-[#727d73] lg:text-[80px] font-extrabold font-['Inter'] italic">
